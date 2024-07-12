82c7433c2885012c68faa961d499f83bdb23d03e
Diogo Trindade <diogotr7@gmail.com>
2024-07-12 15:42:36 +0100

    check how long reading p4k takes

--- src/StarBreaker.Profile/Program.cs
+++ src/StarBreaker.Profile/Program.cs
@@ -1,14 +1,31 @@
-﻿using StarBreaker.CryChunkFile;
+﻿using System.Diagnostics;
+using System.IO.Compression;
+using StarBreaker.CryChunkFile;
+using StarBreaker.CryXmlB;
+using StarBreaker.P4k;
 
+// ReSharper disable UnusedMember.Local
 namespace StarBreaker.Profile;
 
 public static class Program
 {
+    private const string p4k = @"C:\Program Files\Roberts Space Industries\StarCitizen\LIVE\Data.p4k";
+    private const string socpak = @"D:\StarCitizenExport\Data\ObjectContainers\PU\loc\flagship\stanton\newbab\newbab_all.socpak";
+    private const string depot = @"D:\StarCitizenExport";
+    private const string problematic = @"D:\newbab_all.rmxml";
+
     public static void Main(string[] args)
     {
-        const string p4k = @"C:\Program Files\Roberts Space Industries\StarCitizen\LIVE\Data.p4k";
-        const string depot = @"D:\StarCitizenExport";
+        //var cxml = new CryXml(File.ReadAllBytes(problematic));
+        //File.WriteAllText("output.xml", cxml.ToXmlString());
 
+        //ExtrackSocPak();
+        TimeP4kExtract();
+        //ExtractChunkFiles();
+    }
+
+    private static void ExtractChunkFiles()
+    {
         var exts = new[]
         {
             "aim",
@@ -30,14 +47,15 @@ public static class Program
 
         var filteredFiles = Directory.EnumerateFiles(depot, "*", SearchOption.AllDirectories)
             .Where(file => exts.Any(ext => file.EndsWith(ext, StringComparison.InvariantCultureIgnoreCase)));
-            // .Where(ChunkFile.IsChunkFile)
-            // .Select(x => x.Split('.').Last()).Distinct().ToArray();
 
         foreach (var file in filteredFiles)
         {
             try
             {
-                ExtractChunkFile(file);
+                if (!ChunkFile.TryOpen(File.ReadAllBytes(file), out var chunkFile))
+                    throw new Exception("Failed to open chunk file");
+
+                chunkFile!.WriteXmlTo(Console.Out);
             }
             catch (Exception e)
             {
@@ -46,12 +64,46 @@ public static class Program
             }
         }
     }
-
-    private static void ExtractChunkFile(string chr)
+    
+    private static void ExtrackSocPak()
     {
-        if (!ChunkFile.TryOpen(File.ReadAllBytes(chr), out var chunkFile))
-            throw new Exception("Failed to open chunk file");
+        using var zip = new ZipArchive(File.OpenRead(socpak));
+        foreach (var entry in zip.Entries)
+        {
+            var stream = entry.Open();
+            var bytes = new byte[entry.Length];
+            stream.Read(bytes);
+            if (CryXml.TryOpen(bytes, out var cryXml))
+            {
+                var s = cryXml.ToXmlString();
+                Console.WriteLine(cryXml);
+            }
 
-        chunkFile!.WriteXmlTo(Console.Out);
+            Console.WriteLine(entry.FullName);
+        }
+    }
+
+    private static void TimeP4kExtract()
+    {
+        var sw1 = Stopwatch.StartNew();
+        var p4kFile = new P4kFile(p4k);
+        sw1.Stop();
+        
+        Console.WriteLine($"Took {sw1.ElapsedMilliseconds}ms to load {p4kFile.Entries.Length} entries");
+    }
+    
+    private static void TimeZipNode()
+    {        
+        var p4kFile = new P4kFile(p4k);
+
+        var times = new List<long>();
+        for (var i = 0; i < 8; i++)
+        {
+            var sw = Stopwatch.StartNew();
+            var tree = new ZipNode(p4kFile.Entries);
+            times.Add(sw.ElapsedMilliseconds);
+        }
+
+        Console.WriteLine($"Average: {times.Average()}ms");
     }
 }
---
+++ src/StarBreaker.Profile/ZipNode.cs
@@ -0,0 +1,57 @@
+﻿using StarBreaker.P4k;
+
+namespace StarBreaker.Profile;
+
+public class ZipNode
+{
+    private readonly Dictionary<int, ZipNode> _directory;
+    private readonly List<ZipEntry> _files;
+    private readonly string _name;
+    
+    public string Name => _name;
+    public IDictionary<int, ZipNode> Directory => _directory;
+    public List<ZipEntry> Files => _files;
+
+    public ZipNode(string name)
+    {
+        _name = name;
+        _files = [];
+        _directory = [];
+    }
+
+    public ZipNode(ReadOnlySpan<ZipEntry> zipEntries)
+    {
+        _name = "";
+        _directory = [];
+        _files = [];
+        
+        Span<Range> ranges = new Range[20];
+        foreach (var zipEntry in zipEntries)
+        {
+            var current = this;
+            var name = zipEntry.Name.AsSpan();
+            var rangeCount = name.Split(ranges, '\\');
+
+            for (var index = 0; index < rangeCount - 1; index++)
+            {
+                var part = name[ranges[index]];
+                var partHashCode = string.GetHashCode(part);
+                
+                if (!current._directory.TryGetValue(partHashCode, out var value))
+                {
+                    value = new ZipNode(part.ToString());
+                    current._directory[partHashCode] = value;
+                }
+
+                current = value;
+            }
+            
+            current._files.Add(zipEntry);
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} | {Directory.Count}";
+    }
+}
