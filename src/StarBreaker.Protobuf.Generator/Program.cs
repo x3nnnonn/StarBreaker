283280243c30b8d183ac09c2ad1dfe965900cc31
Diogo Trindade <diogotr7@gmail.com>
2024-10-10 16:56:33 +0100

    extract protobuf from exe

---
+++ src/StarBreaker.Protobuf.Generator/Program.cs
@@ -0,0 +1,7 @@
+﻿using StarBreaker.Protobuf.Generator;
+
+const string starCitizenExe = @"C:\Program Files\Roberts Space Industries\StarCitizen\PTU\Bin64\StarCitizen.exe";
+const string sourcePath = "../../../../StarBreaker.Protobuf/protos";
+
+//Generates c sharp protobuf source files inside the StarBreaker.Protobuf project.
+new ReadFromBinary(starCitizenExe).Generate(Path.GetFullPath(sourcePath));
