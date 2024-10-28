3134fc2cdffcad7c4b69edc687fe80c514e91228
Diogo Trindade <diogotr7@gmail.com>
2024-10-28 17:49:49 +0000

    cursed dds

---
+++ src/StarBreaker.Sandbox/DdsUnsplit.cs
@@ -0,0 +1,11 @@
+﻿using StarBreaker.Dds;
+
+namespace StarBreaker.Sandbox;
+
+public static class DdsUnsplit
+{
+    public static void Run()
+    {
+        var dds = DdsFile.FromFile(@"C:\Scratch\StarCitizen\p4k\Data\Textures\planets\global\stanton\stanton1\stanton1_clouds_global.dds");
+    }
+}
