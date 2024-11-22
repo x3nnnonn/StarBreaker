c57e32d57f8fb89a4c91796acf243923aac04914
Diogo Trindade <diogotr7@gmail.com>
2024-11-22 22:37:58 +0000

    add initial filesystem project

---
+++ src/StarBreaker.FileSystem/FileSystem.cs
@@ -0,0 +1,14 @@
+﻿namespace StarBreaker.FileSystem;
+
+public sealed class FileSystem : IFileSystem
+{
+    public static readonly FileSystem Instance = new();
+    
+    public IEnumerable<string> GetFiles(string path) => Directory.EnumerateFiles(path);
+
+    public IEnumerable<string> GetDirectories(string path) => Directory.EnumerateDirectories(path);
+
+    public bool Exists(string path) => File.Exists(path) || Directory.Exists(path);
+
+    public Stream OpenRead(string path) => File.OpenRead(path);
+}
---
+++ src/StarBreaker.FileSystem/ZipFileSystem.cs
@@ -0,0 +1,42 @@
+﻿using System.IO.Compression;
+
+namespace StarBreaker.FileSystem;
+
+public sealed class ZipFileSystem : IFileSystem
+{
+    private readonly ZipArchive _archive;
+
+    public ZipFileSystem(ZipArchive archive)
+    {
+        _archive = archive;
+    }
+
+    public IEnumerable<string> GetFiles(string path)
+    {
+        return _archive.Entries
+            .Where(entry => entry.FullName.StartsWith(path) && !entry.FullName.EndsWith('/'))
+            .Select(entry => entry.FullName);
+    }
+
+    public IEnumerable<string> GetDirectories(string path)
+    {
+        return _archive.Entries
+            .Where(entry => entry.FullName.StartsWith(path) && entry.FullName.EndsWith('/'))
+            .Select(entry => entry.FullName);
+    }
+
+    public bool Exists(string path)
+    {
+        return _archive.Entries.Any(entry => entry.FullName == path);
+    }
+
+    public Stream OpenRead(string path)
+    {
+        var entry = _archive.GetEntry(path);
+        
+        if (entry == null)
+            throw new FileNotFoundException("File not found in archive.", path);
+        
+        return entry.Open();
+    }
+}
