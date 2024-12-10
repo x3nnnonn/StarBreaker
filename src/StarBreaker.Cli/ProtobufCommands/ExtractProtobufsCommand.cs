955f5fbb498f16980aa401b3f2ac821bb4ad612f
Diogo Trindade <diogotr7@gmail.com>
2024-12-10 17:24:35 +0000

    many protobuf improvements

---
+++ src/StarBreaker.Cli/ProtobufCommands/ExtractProtobufsCommand.cs
@@ -0,0 +1,26 @@
+﻿using CliFx;
+using CliFx.Attributes;
+using CliFx.Infrastructure;
+using StarBreaker.Protobuf;
+
+namespace StarBreaker.Chf;
+
+[Command("proto-extract", Description = "Extracts protobuf definitions from the Star Citizen executable.")]
+public class ExtractProtobufsCommand : ICommand
+{
+    [CommandOption("input", 'i', Description = "The path to the Star Citizen executable.")]
+    public required string Input { get; init; } = string.Empty;
+
+    [CommandOption("output", 'o', Description = "The path to the output directory.")]
+    public required string Output { get; init; } = string.Empty;
+
+    public ValueTask ExecuteAsync(IConsole console)
+    {
+        console.Output.WriteLine("Extracting protobuf definitions...");
+        var extractor = new ProtobufExtractor(Input);
+        extractor.WriteProtos(Output, p => !p.Name.StartsWith("google/protobuf"));
+        console.Output.WriteLine("Wrote {0} protobuf definitions to {1}", extractor.FileDescriptorProtos.Count, Output);
+        
+        return default;
+    }
+}
--- src/StarBreaker.Protobuf/ProtobufExtractor.cs
+++ src/StarBreaker.Protobuf/ProtobufExtractor.cs
@@ -5,47 +5,102 @@ namespace StarBreaker.Protobuf;
 
 public class ProtobufExtractor
 {
-    private readonly string _file;
+    public List<FileDescriptorProto> FileDescriptorProtos { get; }
 
     public ProtobufExtractor(string file)
     {
         if (!File.Exists(file))
             throw new FileNotFoundException("File not found", file);
 
-        _file = file;
-    }
-    
-    private static bool IsRelevant(FileDescriptor f)
-    {
-        //sc and cig are fairly obvious, the other one seems to be arena commander statistics of some kind? a bit odd that it's different.
-        return f.Package.StartsWith("sc", StringComparison.InvariantCultureIgnoreCase) ||
-               f.Package.StartsWith("cig", StringComparison.InvariantCultureIgnoreCase) ||
-               string.Equals(f.Package, "service_statistics", StringComparison.InvariantCultureIgnoreCase);
+        // first we get the raw file descriptors as decoded bytes from the exe
+        var rawFileDescriptorProtos = GetDescriptorsFromFile(file);
+
+        // some protos are duplicates (google well known types), so we deduplicate them. Keeps the largest one.
+        var dedupedFileDescriptorProtos = DeduplicateFileDescriptors(rawFileDescriptorProtos);
+
+        // we need them to be in order of dependency for a valid FileDescriptorSet. Do it here to guarantee valid output.
+        FileDescriptorProtos = OrderByDependency(dedupedFileDescriptorProtos);
     }
 
-    public void Generate(string protoPath)
+    public void WriteProtos(string protoPath, Func<FileDescriptor, bool>? filter = null)
     {
-        var protos = GetDescriptorsFromFile();
-        var fileDescriptors = FromFileDescriptorProtos(protos);
+        // This next step is kind of stupid, but it's the only format FileDescriptor accepts
+        var protoByteStrings = FileDescriptorProtos.Select(x => x.ToByteString());
+        var fileDescriptors = FileDescriptor.BuildFromByteStrings(protoByteStrings);
+
         var targetFolder = Directory.CreateDirectory(protoPath);
-        
-        foreach (var fileDescriptor in fileDescriptors.Where(IsRelevant))
+
+        var opts = new DynamicGrpcPrinterOptions
         {
+            FullyQualified = true
+        };
+        
+        foreach (var fileDescriptor in fileDescriptors)
+        {
+            if (filter != null && !filter(fileDescriptor))
+                continue;
+            
             var path = Path.Combine(targetFolder.FullName, fileDescriptor.Name);
             var dir = Path.GetDirectoryName(path);
-            if (!string.IsNullOrWhiteSpace(dir)) Directory.CreateDirectory(dir);
-            var protoText = fileDescriptor.ToProtoString(new DynamicGrpcPrinterOptions()
-            {
-                FullyQualified = false
-            });
-            File.WriteAllText(path, protoText);
+
+            if (!string.IsNullOrWhiteSpace(dir))
+                Directory.CreateDirectory(dir);
+
+            File.WriteAllText(path, fileDescriptor.ToProtoString(opts));
         }
     }
 
-    private List<FileDescriptorProto> GetDescriptorsFromFile()
+    public void WriteDescriptorSet(string descriptorSetPath)
+    {
+        var directory = Path.GetDirectoryName(Path.GetFullPath(descriptorSetPath));
+        if (directory == null)
+            throw new InvalidOperationException("Directory must be specified for descriptor set.");
+        
+        Directory.CreateDirectory(directory);
+
+        var set = new FileDescriptorSet();
+        set.File.AddRange(FileDescriptorProtos);
+
+        using var output = File.Create(descriptorSetPath);
+        set.WriteTo(output);
+    }
+
+    private static IEnumerable<FileDescriptorProto> DeduplicateFileDescriptors(IEnumerable<FileDescriptorProto> protos)
+    {
+        return protos
+            .GroupBy(x => x.Name)
+            .Select(x =>
+                x.MaxBy(y => y.CalculateSize()) ?? throw new InvalidOperationException("MaxBy returned null")
+            );
+    }
+
+    private static List<FileDescriptorProto> OrderByDependency(IEnumerable<FileDescriptorProto> unordered)
+    {
+        var resolved = new HashSet<string>();
+        var orderedList = new List<FileDescriptorProto>();
+        var unorderedList = new List<FileDescriptorProto>(unordered);
+
+        while (unorderedList.Count > 0)
+        {
+            var proto = unorderedList.FirstOrDefault(x => x.Dependency.All(dep => resolved.Contains(dep)));
+            if (proto == null)
+            {
+                throw new InvalidOperationException(
+                    $"Invalid proto dependencies. Unable to resolve remaining protos [{string.Join(",", unorderedList.Select(x => x.Name))}] that don't have all their dependencies available.");
+            }
+
+            resolved.Add(proto.Name);
+            unorderedList.Remove(proto);
+            orderedList.Add(proto);
+        }
+
+        return orderedList;
+    }
+
+    private static List<FileDescriptorProto> GetDescriptorsFromFile(string file)
     {
         var protos = new List<FileDescriptorProto>();
-        var span = File.ReadAllBytes(_file).AsSpan();
+        var span = File.ReadAllBytes(file).AsSpan();
 
         var fileLength = span.Length;
 
@@ -93,43 +148,7 @@ public class ProtobufExtractor
         return protos;
     }
 
-    public static IReadOnlyList<FileDescriptor> FromFileDescriptorProtos(IEnumerable<FileDescriptorProto> protos)
-    {
-        // Step 1 - FileDescriptor.BuildFromByteStrings doesn't support passing files in random order, so we need to reorder them with protos
-        // It is very unfortunate, as we are doubling the deserialization of FileDescriptorProto
-        var resolved = new HashSet<string>();
-        var orderedList = new List<ByteString>();
-
-        var unorderedList = protos.GroupBy(x => x.Name).Select(x =>
-        {
-            var count = x.Count();
-            return count switch
-            {
-                0 => throw new InvalidOperationException("This should never happen"),
-                1 => x.First(),
-                _ => x.MaxBy(y => y.ToByteArray().Length)!
-            };
-        }).ToList();
-
-        while (unorderedList.Count > 0)
-        {
-            var proto = unorderedList.FirstOrDefault(x => x.Dependency.All(dep => resolved.Contains(dep)));
-            if (proto == null)
-            {
-                throw new InvalidOperationException(
-                    $"Invalid proto dependencies. Unable to resolve remaining protos [{string.Join(",", unorderedList.Select(x => x.Name))}] that don't have all their dependencies available.");
-            }
-
-            resolved.Add(proto.Name);
-            unorderedList.Remove(proto);
-            orderedList.Add(proto.ToByteString());
-        }
-
-        // Step 2 - Build FileDescriptor from properly ordered list
-        return FileDescriptor.BuildFromByteStrings(orderedList);
-    }
-    
-    public static ulong DecodeVarInt(Span<byte> span, int pos, out ulong outPos)
+    private static ulong DecodeVarInt(Span<byte> span, int pos, out ulong outPos)
     {
         var result = 0;
         var shift = 0;
