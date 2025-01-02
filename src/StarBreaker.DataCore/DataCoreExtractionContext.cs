624925a12e1d303e9b3f6027969b77b69f0e49ba
Diogo Trindade <diogotr7@gmail.com>
2025-01-02 21:58:02 +0000

    small cleanup

---
+++ src/StarBreaker.DataCore/DataCoreExtractionContext.cs
@@ -0,0 +1,13 @@
+﻿namespace StarBreaker.DataCore;
+
+public sealed class DataCoreExtractionContext
+{
+    public HashSet<(int structIndex, int instanceIndex)> Tracker { get; }
+    public string FileName { get; }
+
+    public DataCoreExtractionContext(string fileName)
+    {
+        Tracker = [];
+        FileName = fileName;
+    }
+}
