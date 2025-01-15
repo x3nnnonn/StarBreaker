d897ea0737c2d3b5d811d8fb89dc5cd21b2aa800
Diogo Trindade <diogotr7@gmail.com>
2025-01-15 22:56:10 +0000

    add util timer

---
+++ src/StarBreaker.Common/Utilities/TimeLogger.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace StarBreaker.Common;
+
+public struct TimeLogger
+{
+    private long timestamp;
+
+    public TimeLogger()
+    {
+        timestamp = Stopwatch.GetTimestamp();
+    }
+
+    public void LogReset(string message)
+    {
+        var elapsed = Stopwatch.GetElapsedTime(timestamp);
+        Console.WriteLine($"{message}: {elapsed.TotalMilliseconds}ms");
+        timestamp = Stopwatch.GetTimestamp();
+    }
+
+    public void LogContinue(string message)
+    {
+        var elapsed = Stopwatch.GetElapsedTime(timestamp);
+        Console.WriteLine($"{message}: {elapsed.TotalMilliseconds}ms");
+    }
+}
