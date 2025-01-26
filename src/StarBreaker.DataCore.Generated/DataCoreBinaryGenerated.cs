e1729ec5b2a41bc7322b3365bff4046c88720af9
Diogo Trindade <diogotr7@gmail.com>
2025-01-26 18:17:48 +0000

    wrap record data in a record type

--- src/StarBreaker.DataCore.Generated/DataCoreBinaryGenerated.cs
+++ src/StarBreaker.DataCore.Generated/DataCoreBinaryGenerated.cs
@@ -5,7 +5,7 @@ namespace StarBreaker.DataCoreGenerated;
 
 //TODO: come up with a way of hashing the types we generated, then verify that the datacore file we're reading matches the types we have.
 // if they don't match we *WILL* fail to read it. We should throw before this.
-public sealed partial class DataCoreBinaryGenerated : IDataCoreBinary<IDataCoreReadable>
+public sealed partial class DataCoreBinaryGenerated : IDataCoreBinary<DataCoreTypedRecord>
 {
     public DataCoreDatabase Database { get; }
 
@@ -14,14 +14,14 @@ public sealed partial class DataCoreBinaryGenerated : IDataCoreBinary<IDataCoreR
         Database = database;
     }
 
-    public IDataCoreReadable GetFromMainRecord(DataCoreRecord record)
+    public DataCoreTypedRecord GetFromMainRecord(DataCoreRecord record)
     {
         var data = ReadFromRecord(record.StructIndex, record.InstanceIndex);
 
         if (data == null)
             throw new InvalidOperationException($"Failed to read data from record {record}");
 
-        return data;
+        return new DataCoreTypedRecord(record.GetFileName(Database), record.GetName(Database), record.Id, data);
     }
 
     public void SaveToFile(DataCoreRecord record, string path)
