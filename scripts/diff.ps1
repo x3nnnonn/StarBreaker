cdacf11ad1f8b40c35d6bc630fbf704112e6c913
Diogo Trindade <diogotr7@gmail.com>
2025-01-29 23:53:45 +0000

    add diff script

---
+++ scripts/diff.ps1
@@ -0,0 +1,16 @@
+#Run StarBreaker.Cli diff with the correct parameters
+$starBreakerCliCsproj = "C:\Development\StarCitizen\StarBreaker\src\StarBreaker.Cli\StarBreaker.Cli.csproj"
+$starCitizenBasePath = "C:\Program Files\Roberts Space Industries\StarCitizen"
+#$channel = "LIVE"
+#$channel = "PTU"
+$channel = "4.0_PREVIEW"
+$starCitizenPath = "$starCitizenBasePath\$channel"
+$gitRepoPath = "C:\Development\StarCitizen\StarCitizenDiff2"
+
+dotnet run --project $starBreakerCliCsproj -- diff -g $starCitizenPath -o $gitRepoPath -k false -f json
+
+#Set-Location $gitRepoPath
+#git add .
+#git commit --author="StarBreaker <>" -m "StarBreaker diff"
+#
+#Write-Host "Diff completed"
