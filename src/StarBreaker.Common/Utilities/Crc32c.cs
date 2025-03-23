6bbc053e0de0ab2ce503f64377c5f27f48a1d112
Diogo Trindade <diogotr7@gmail.com>
2025-03-23 21:54:34 +0000

    speed up crc32c

--- src/StarBreaker.Common/Utilities/Crc32c.cs
+++ src/StarBreaker.Common/Utilities/Crc32c.cs
@@ -1,27 +1,47 @@
-﻿using System.Buffers;
-using System.Collections;
-using System.Numerics;
-using System.Runtime.CompilerServices;
+﻿using System.Numerics;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace StarBreaker.Common;
 
 public static class Crc32c
 {
-    public static uint FromSpan(ReadOnlySpan<byte> data)
+    public static uint FromSpan(ReadOnlySpan<byte> data, uint seed = 0u)
     {
-        var acc = 0xFFFFFFFFu;
-        
-        foreach (ref readonly var t in data)
+        var acc = ~seed;
+        var remaining = data.Length;
+
+        if (remaining >= sizeof(ulong))
         {
-            acc = BitOperations.Crc32C(acc, t);
+            var span = MemoryMarshal.Cast<byte, ulong>(data);
+
+            foreach (var u in span)
+                acc = BitOperations.Crc32C(acc, u);
+
+            remaining -= span.Length * sizeof(ulong);
+        }
+
+        if (remaining >= sizeof(uint))
+        {
+            var span = MemoryMarshal.Cast<byte, uint>(data[^remaining..]);
+
+            foreach (var u in span)
+                acc = BitOperations.Crc32C(acc, u);
+
+            remaining -= span.Length * sizeof(uint);
+        }
+
+        if (remaining > 0)
+        {
+            foreach (var t in data[^remaining..])
+                acc = BitOperations.Crc32C(acc, t);
         }
 
         return ~acc;
     }
-    
+
     public static uint FromSpan(Span<byte> data) => FromSpan((ReadOnlySpan<byte>)data);
-    
+
     public static uint FromString(string data)
     {
         var length = Encoding.UTF8.GetByteCount(data);
