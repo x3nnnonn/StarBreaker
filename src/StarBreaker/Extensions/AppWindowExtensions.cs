00331ce6be1ca18ed5c0638bff97e91b31f528d0
Diogo Trindade <diogotr7@gmail.com>
2025-04-19 17:45:19 +0100

    UI clean

--- src/StarBreaker/Extensions/AppWindowExtensions.cs
+++ src/StarBreaker/Extensions/AppWindowExtensions.cs
@@ -1,18 +1,18 @@
 ﻿using System.Runtime.InteropServices;
 using Avalonia.Controls;
 using Avalonia.Media;
-using FluentAvalonia.UI.Windowing;
 
 namespace StarBreaker.Extensions;
 
 public static class AppWindowExtensions
 {
-    public static void EnableMicaTransparency(this AppWindow window)
+    public static void EnableMicaTransparency(this Window window)
     {
+        //return;
         if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             return;
         
         window.TransparencyLevelHint = [WindowTransparencyLevel.Mica];
-        window.Background = new SolidColorBrush(new Color(128, 0, 0, 0));
+        window.Background = new SolidColorBrush(new Color(64, 0, 0, 0));
     }
 }
