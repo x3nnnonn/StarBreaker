ee86a75d8dd408658175709aeec085ed361bed1a
Diogo Trindade <diogotr7@gmail.com>
2025-05-04 19:27:17 +0100

    very basic bnk file support

--- src/StarBreaker.Sandbox/WwiseSandbox.cs
+++ src/StarBreaker.Sandbox/WwiseSandbox.cs
@@ -7,6 +7,7 @@ public static class WwiseSandbox
     public static void Run()
     {
         var bnk = BnkFile.Open(File.OpenRead(@"D:\StarCitizen\P4k\Data\Sounds\wwise\SSAM_CRUS_StarFighter.bnk"));
+        bnk.ExtractWemFiles(@"D:\StarCitizen\Wems");
         Console.WriteLine(bnk);
     }
 }
