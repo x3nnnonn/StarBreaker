2f1a1795ac445a118e7da12c9a640d5cafd04654
Diogo Trindade <diogotr7@gmail.com>
2025-05-04 18:26:22 +0100

    scaffold bnk

---
+++ src/StarBreaker.Sandbox/WwiseSandbox.cs
@@ -0,0 +1,12 @@
+﻿using StarBreaker.Wwise.Bnk;
+
+namespace StarBreaker.Sandbox;
+
+public static class WwiseSandbox
+{
+    public static void Run()
+    {
+        var bnk = BnkFile.Open(File.OpenRead(@"D:\StarCitizen\P4k\Data\Sounds\wwise\SSAM_CRUS_StarFighter.bnk"));
+        Console.WriteLine(bnk);
+    }
+}
